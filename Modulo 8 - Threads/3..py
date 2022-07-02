35c412a823eb05a30b458c89a478da8a6030ec4a
João Pedro Barreto <joaopedro.bcalmeida@gmail.com>
2022-07-02 10:58:43 -0300

    Módulo 8 (Ex. 3, a)

---
+++ Modulo 8 - Threads/3..py
@@ -0,0 +1,14 @@
+#3. Responda as questões a seguir: 
+#a) O que é o problema do produtor consumidor e como podemos resolver este problema com 
+#threads? 
+
+""" - O problema do Produtor Consumidor, resumido de maneira simplificada, é muito comum
+no contexto da programação com threads. Ele acontece porque, se feito de maneira incorreta,
+o Consumidor pode tentar consumir informações que ainda não foram produzidas pelo Produtor. 
+Isso acontece pois, a partir do uso de threads, pode-se criar códigos concorrente, ou seja, 
+códigos que são executados simultaneamente. Dessa maneira, haverá um erro, já que o cosumidor
+tentará consumir dados que ainda não existem.
+""" 
+
+#b) Indique mais um problema clássico da computação e como podemos resolve-los com threads 
+#explicando de forma detalhada 
