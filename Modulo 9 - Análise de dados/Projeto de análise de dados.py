68b84fa0c506257962d358cbb380d11e190fccbf
João Pedro Barreto <joaopedro.bcalmeida@gmail.com>
2022-07-06 06:47:10 -0300

    small change on modulo 9

--- Modulo 9 - Análise de dados/Projeto de análise de dados.py
+++ Modulo 9 - Análise de dados/Projeto de análise de dados.py
@@ -4,6 +4,3 @@ import pandas as pd
 df = pd.read_csv("dadosgovbr---2014.csv",sep = ';', encoding="latin-1")
 # visualizar alguns dados da tabela carregada
 df.head()
-sudo apt install python3.8-venv
-python3 -m venv venv38
-
