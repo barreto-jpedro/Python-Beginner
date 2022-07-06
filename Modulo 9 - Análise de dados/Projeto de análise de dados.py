6bdb60a63fa01a05952c97e1cac65144f692b0e3
João Pedro Barreto <joaopedro.bcalmeida@gmail.com>
2022-07-06 06:46:25 -0300

    Modulo 9 - First commit

---
+++ Modulo 9 - Análise de dados/Projeto de análise de dados.py
@@ -0,0 +1,9 @@
+# importa a biblioteca pandas
+import pandas as pd
+# armazena os dados da tabela em uma estrutura tipo data frame
+df = pd.read_csv("dadosgovbr---2014.csv",sep = ';', encoding="latin-1")
+# visualizar alguns dados da tabela carregada
+df.head()
+sudo apt install python3.8-venv
+python3 -m venv venv38
+
