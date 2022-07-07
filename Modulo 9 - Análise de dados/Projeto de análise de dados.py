8b998733c2e4fcd138f9977ece065c9e76c78b6e
João Pedro Barreto <joaopedro.bcalmeida@gmail.com>
2022-07-07 17:08:07 -0300

    Modulo 9 improvments

--- Modulo 9 - Análise de dados/Projeto de análise de dados.py
+++ Modulo 9 - Análise de dados/Projeto de análise de dados.py
@@ -1,6 +1,7 @@
 # importa a biblioteca pandas
 import pandas as pd
 # armazena os dados da tabela em uma estrutura tipo data frame
-df = pd.read_csv("dadosgovbr---2014.csv",sep = ';', encoding="latin-1")
+df = pd.read_csv("Dados_projeto_modulo_9.csv",sep = ';', encoding="latin-1")
 # visualizar alguns dados da tabela carregada
 df.head()
+print(df.Região.value_counts())
