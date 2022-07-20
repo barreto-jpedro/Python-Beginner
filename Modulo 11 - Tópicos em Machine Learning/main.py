0b1edb8714f7fec14a2145754c9c635cf6e696b3
João Pedro Barreto <joaopedro.bcalmeida@gmail.com>
2022-07-20 17:22:13 -0300

    Last exercise

---
+++ Modulo 11 - Tópicos em Machine Learning/main.py
@@ -0,0 +1,30 @@
+#1 – Crie um modelo de classificação de imagens, a partir da importação da base
+#de dados do Sci-kit Learn. Este modelo deve ter as seguintes características:
+#a) Utilize a base de dados Mnist Dataset
+#b) Divida a base de treinamento e teste em 80/20.
+#c) Utilize a técnica de Cross Validation (K-fold = 5)
+#d) Utilize a técnica de Random Search ou Grid Search para escolha dos melhores
+#parâmetros
+#e) Faça a impressão da matriz de confusão para o modelo
+
+#2 – Após a etapa de treinamento, analise o modelo respondendo as questões a
+#seguir:
+#a) Qual a acurácia do modelo? Justifique
+#b) O modelo teve uma boa performance? Justifique
+#c) Quais foram os melhores parâmetros escolhidos? Justifique
+#d) Quais as principais dificuldades encontradas para a criação do seu modelo
+#de classificação?
+
+#3 – Faça uma pesquisa para conhecer um pouco mais sobre o universo de
+#Machine Learning e responda as questões a seguir:
+#a) Qual é a diferença entre um parâmetro de modelo e um algoritmo de
+#aprendizagem de hiperparâmetro?
+#b) Você pode citar quatro dos principais desafios do aprendizado de
+#máquina?
+#c) Se o seu modelo tem um ótimo desempenho nos dados de treinamento,
+#mas generaliza mal para novas instâncias, o que está acontecendo? Você
+#pode citar três soluções possíveis?
+#d) O que é um conjunto de teste e por que você deveria usa-lo?
+#e) Qual é o propósito de um conjunto de validação?
+#f) O que pode dar errado se você ajustar hiperparâmetro usando o conjunto
+#de teste?
